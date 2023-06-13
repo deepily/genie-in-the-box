191b835b6f22e69a3668a37dd86d7c6b7f12e074
Deepily <42214371+deepily@users.noreply.github.com>
2023-06-13 13:20:36 -0400

    Objectify accuracy code

--- src/prompts/accuracy.py
+++ src/prompts/accuracy.py
@@ -1,18 +1,31 @@
 from src import genie_client as gc
+from src.lib import util as du
 
 import json
-def load_json( path ):
+
+
+class Accuracy:
     
-    with open( path, 'r' ) as f:
-        data = json.load( f )
+    def __init__( self, proj_root="/Users/rruiz/Projects/projects-sshfs/genie-in-the-box" ):
+        
+        self.proj_root = proj_root
+        self.prompt    = du.get_file_as_string( proj_root + "/src/prompts/classification-experiment.txt" )
     
-    return data
+    def load_json( path ):
+        
+        with open( path, 'r' ) as f:
+            data = json.load( f )
+        
+        return data
+    
+    
 
 if __name__ == "__main__":
     
     print( "Calculating accuracy..." )
+    accuracy = Accuracy()
     # load data
-    data = load_json( "data/synthetic-data-load-url-current-tab.json" )
+    data = accuracy.load_json( "data/synthetic-data-load-url-current-tab.json" )
     # get total number of prompts
     total = len( data )
     print( "Total number of prompts: {}".format( total ) )
