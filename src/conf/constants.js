d8693bcaa8d33091e880949460b144ac64cc67dd
Deepily <42214371+deepily@users.noreply.github.com>
2023-06-27 13:17:57 -0400

    Tweak JavaScript syntax to make string constant declarations consistent and parsable

--- src/conf/constants.js
+++ src/conf/constants.js
@@ -1,4 +1,6 @@
 console.log( "Loading constants.js..." );
+
+// These constants will be parsed by the multimodal munger object, and should be listed from top to bottom by longest string first.
 export const VOX_CMD_CUT            = "cut";
 export const VOX_CMD_COPY           = "copy";
 export const VOX_CMD_PASTE          = "paste from clipboard";
@@ -23,7 +25,7 @@ export const VOX_CMD_VIEW_CHEAT_SHEET= "view cheat sheet";
 
 export const VOX_CMD_ZOOM_IN        = "zoom in";
 export const VOX_CMD_ZOOM_OUT       = "zoom out";
-export const VOX_CMD_ZOOM_RESET     = "zoom reset"
+export const VOX_CMD_ZOOM_RESET     = "zoom reset";
 
 export const VOX_CMD_MODE_RESET     = "reset";
 export const VOX_CMD_MODE_EXIT      = "exit";
@@ -46,11 +48,11 @@ export const VOX_CMD_SEARCH_CLIPBOARD_GOOGLE_SCHOLAR = "search google scholar us
 
 export const MULTIMODAL_CONTACT_INFO  = "multimodal contact information";
 export const MULTIMODAL_TEXT_PYTHON   = "multimodal python punctuation";
-export const MULTIMODAL_TEXT_EMAIL    = "multimodal text email"
+export const MULTIMODAL_TEXT_EMAIL    = "multimodal text email";
 export const STEM_MULTIMODAL_EDITOR   = "multimodal editor";
-export const STEM_MULTIMODAL_SERVER_SEARCH = "multimodal server search"
+export const STEM_MULTIMODAL_SERVER_SEARCH = "multimodal server search";
 
-export const VOX_CMD_RUN_PROMPT           = "run prompt"
+export const VOX_CMD_RUN_PROMPT           = "run prompt";
 export const VOX_CMD_SUFFIX_FROM_FILE     = "from file";
 export const VOX_CMD_SUFFIX_FROM_CLIPBOARD= "from clipboard";
 
@@ -66,7 +68,7 @@ export const EDITOR_URL                = "http://127.0.0.1:8080/genie-plugin-fir
 export const BUCKET_URL                = "http://127.0.0.1:8080/genie-plugin-firefox/html/blank.html";
 export const CONSTANTS_URL             = "http://127.0.0.1:8080/genie-plugin-firefox/js/constants.js";
 export const SEARCH_URL_GOOGLE         = "https://www.google.com/search";
-export const SEARCH_URL_GOOGLE_SCHOLAR = "https://scholar.google.com/scholar"
+export const SEARCH_URL_GOOGLE_SCHOLAR = "https://scholar.google.com/scholar";
 export const SEARCH_URL_DDG            = "https://www.duckduckgo.com/";
 
 export const ZOOM_INCREMENT = 0.075;
