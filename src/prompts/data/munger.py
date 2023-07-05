e9e903d5be3cdb81f36dd2d12d255d78bae17521
Deepily <42214371+deepily@users.noreply.github.com>
2023-07-05 11:37:15 -0400

    Re-factor line number removal function

--- src/prompts/data/munger.py
+++ src/prompts/data/munger.py
@@ -1,17 +1,22 @@
 import src.lib.util as du
 
-# Create main method
-if __name__ == "__main__":
-
-    path = du.get_project_root() + "/src/prompts/data/synthetic-data-search-in-current-tab.txt"
+def remove_line_numbers( path ):
+    
     lines = du.get_file_as_list( path, clean=True )
     
-    print( "Total number of prompts: {}".format( len( lines ) ) )
+    print( "Total number of lines: [{}]".format( len( lines ) ) )
     
+    # This assumes "number dot space" formatting plus remove extraneous double quotes
     lines = [ line.split( ". " )[ 1 ].replace( '"', '' ) for line in lines ]
     
     for line in lines[ 0:5 ]:
         print( line )
-
+    
     # Write lines back to the same file
     du.write_lines_to_file( path, lines )
+
+# Create main method
+if __name__ == "__main__":
+
+    # remove_line_numbers( path = du.get_project_root() + "/src/prompts/data/synthetic-data-search-in-current-tab.txt" )
+    pass
