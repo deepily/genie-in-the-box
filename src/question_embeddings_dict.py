14a840fb76e81b939b96ccedd15c1405323daa92
rruiz <42214371+deepily@users.noreply.github.com>
2023-08-22 12:00:39 -0400

    Disable serialization of dictionary while loading pickled dictionary

--- src/question_embeddings_dict.py
+++ src/question_embeddings_dict.py
@@ -6,20 +6,29 @@ import lib.util as du
 class QuestionEmbeddingsDict( dict ):
     
     PATH_TO_DICT = os.path.join( du.get_project_root(), "src/conf/long-term-memory/solutions/question-embeddings-dictionary.pickle" )
+    loading = False
     def __init__( self, *args, **kwargs ):
         
         self.update( *args, **kwargs )
         
         if os.path.exists( QuestionEmbeddingsDict.PATH_TO_DICT ):
+            QuestionEmbeddingsDict.loading = True
+            print( "Loading question embeddings dictionary ", end="" )
             with open( QuestionEmbeddingsDict.PATH_TO_DICT, "rb" ) as f:
                 data = pickle.load( f )
                 self.update( data )
-    
+            QuestionEmbeddingsDict.loading = False
+            print( " Done!" )
+            
     def __setitem__( self, key, value ):
         
         super().__setitem__( key, value )
-        self._save()
-        
+        # only save if we are not loading
+        if not QuestionEmbeddingsDict.loading:
+            self._save()
+        else:
+            print( ".", end="" )
+            
     # add method to delete an item
     def __delitem__( self, key ):
         
