00a96cbcac09461ae8b562a5b0585e183c52d7cd
Deepily <42214371+deepily@users.noreply.github.com>
2023-08-21 23:11:42 -0400

    Override delete method and move pickle.dump into its own function, save()

--- src/question_embeddings_dict.py
+++ src/question_embeddings_dict.py
@@ -18,6 +18,16 @@ class QuestionEmbeddingsDict( dict ):
     def __setitem__( self, key, value ):
         
         super().__setitem__( key, value )
+        self._save()
         
+    # add method to delete an item
+    def __delitem__( self, key ):
+        
+        super().__delitem__( key )
+        self._save()
+
+    def _save( self ):
+
+        print( "Pickling question embeddings dictionary..." )
         with open( QuestionEmbeddingsDict.PATH_TO_DICT, "wb" ) as f:
             pickle.dump( self, f )
