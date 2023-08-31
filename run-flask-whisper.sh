d1295c75d9183ed930b17e29fc5dbd7972ed5e6b
Deepily <42214371+deepily@users.noreply.github.com>
2023-08-30 21:07:58 -0400

    Add debug flag to env

--- run-flask-whisper.sh
+++ run-flask-whisper.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-# ¡kluge! fix in the Dockerfile to get the flask app to run
-#pip install flask_cors
-
 cd /var/genie-in-the-box/src
 
-flask --debug run --port 7999 --host 0.0.0.0
+export FLASK_DEBUG=1
+
+flask run --port 7999 --host 0.0.0.0
