d912a40ce729aa6f0b1303e7b3138690db1771b9
Deepily <42214371+deepily@users.noreply.github.com>
2023-09-04 12:56:27 -0400

    Add Rende, re-factored hash generation, and added get copy method

--- src/solution_snapshot.py
+++ src/solution_snapshot.py
@@ -2,9 +2,10 @@ import hashlib
 import os
 import glob
 import json
-from datetime import datetime
 import time
+import copy
 import regex as re
+from datetime import datetime
 
 import lib.util as du
 
@@ -62,7 +63,14 @@ class SolutionSnapshot:
             )
         return response[ "data" ][ 0 ][ "embedding" ]
     
-    def __init__( self, push_counter=-1, question="", answer="", created_date=get_timestamp(), updated_date=get_timestamp(), id_hash="", solution_summary="", code=[],
+    @staticmethod
+    def generate_id_hash( push_counter, run_date ):
+        
+        return hashlib.sha256( (str( push_counter ) + run_date ).encode() ).hexdigest()
+        
+    def __init__( self, push_counter=-1, question="", answer="",
+                  created_date=get_timestamp(), updated_date=get_timestamp(), run_date=get_timestamp(),
+                  id_hash="", solution_summary="", code=[],
                   programming_language="Python", language_version="3.10",
                   question_embedding=[ ], solution_embedding=[ ], code_embedding=[ ],
                   solution_directory="/src/conf/long-term-memory/solutions/", solution_file=None
@@ -77,8 +85,10 @@ class SolutionSnapshot:
         # metadata surrounding the question and the solution
         self.updated_date         = updated_date
         self.created_date         = created_date
+        self.run_date             = run_date
+        
         if id_hash == "":
-            self.id_hash              = hashlib.sha256( (str( self.push_counter ) + self.created_date ).encode() ).hexdigest()
+            self.id_hash              = SolutionSnapshot.generate_id_hash( self.push_counter, self.run_date )
         else:
             self.id_hash              = id_hash
         self.programming_language = programming_language
@@ -151,18 +161,24 @@ class SolutionSnapshot:
             fields_to_exclude = [ field for field in self.__dict__ if field.endswith( '_embedding' ) ]
             data = { field: value for field, value in self.__dict__.items() if field not in fields_to_exclude }
             return json.dumps( data )
+        
+    def get_copy( self ):
+        return copy.copy( self )
     
-    def get_html( self ):
+    def get_html( self, omit_answer=True ):
         
-        if self.answer != "":
-            if len( self.answer ) > 32:
-                self.answer_short = self.answer[ :16 ] + "..."
-            else:
-                self.answer_short = self.answer
+        if omit_answer:
+            return f"<li id='{self.id_hash}'>{self.run_date} Q: {self.question}</li>"
         
-            return f"<li id='{self.id_hash}'>{self.created_date} Q: {self.question} A: {self.answer_short}</li>"
+        # ¡OJO! We may end up not using answer inclusion
         else:
-            return f"<li id='{self.id_hash}'>{self.created_date} Q: {self.question}</li>"
+            if self.answer != "":
+                if len( self.answer ) > 32:
+                    self.answer_short = self.answer[ :16 ] + "..."
+                else:
+                    self.answer_short = self.answer
+        
+            return f"<li id='{self.id_hash}'>{self.run_date} Q: {self.question} A: {self.answer_short}</li>"
     
     def write_to_file( self ):
         
