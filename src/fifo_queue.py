51bda766b833216feb33ad1a8bf749bcea8fd792
Deepily <42214371+deepily@users.noreply.github.com>
2023-09-06 10:34:22 -0400

    Add look up in dictionary by id_hash

--- src/fifo_queue.py
+++ src/fifo_queue.py
@@ -1,37 +1,45 @@
 class FifoQueue:
-    def __init__(self):
-        self.queue = []
+    def __init__( self ):
+        
+        self.queue_list      = [ ]
+        self.queue_dict      = { }
+        self.push_count      = 0
         self.last_queue_size = 0
-        self.push_count = 0
-
-    def push(self, item):
-        self.queue.append(item)
+    
+    def push( self, item ):
+        
+        self.queue_list.append( item )
+        self.queue_dict[ item.id_hash ] = item
         self.push_count += 1
-
-    def get_push_count(self):
+    
+    def get_push_count( self ):
         return self.push_count
-
-    def pop(self):
+    
+    def pop( self ):
         if not self.is_empty():
-            return self.queue.pop(0)
-
-    def head(self):
+            # Remove from ID_hash first
+            del self.queue_dict[ self.queue_list[ 0 ].id_hash ]
+            return self.queue_list.pop( 0 )
+    
+    def head( self ):
         if not self.is_empty():
-            return self.queue[0]
+            return self.queue_list[ 0 ]
         else:
             return None
-
-    def is_empty(self):
-        return len(self.queue) == 0
-
-    def size(self):
-        return len(self.queue)
-
-    def has_changed(self):
+    
+    def get_by_id_hash( self, id_hash ):
+        
+        return self.queue_dict[ id_hash ]
+    
+    def is_empty( self ):
+        return len( self.queue_list ) == 0
+    
+    def size( self ):
+        return len( self.queue_list )
+    
+    def has_changed( self ):
         if self.size() != self.last_queue_size:
             self.last_queue_size = self.size()
             return True
         else:
             return False
-        
-    
