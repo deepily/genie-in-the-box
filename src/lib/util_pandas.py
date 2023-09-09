9d7544297ef8cefb85abf144ca900f11cb1470b4
Deepily <42214371+deepily@users.noreply.github.com>
2023-09-09 00:25:06 -0400

    Create a bucket for commonly used pandas manipulations. TODO: Plunder HC's old python util...

---
+++ src/lib/util_pandas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+import lib.util as du
+
+def cast_to_datetime( df, debug=False ):
+    
+    date_columns = df.select_dtypes( include=[ object ] ).columns.tolist()
+    
+    for column in date_columns:
+        if column.endswith( "_date" ):
+            if pd.api.types.is_string_dtype( df[ column ] ):
+                df[ column ] = pd.to_datetime( df[ column ] )
+    
+    if debug:
+        du.print_banner( "df.dtypes:", prepend_nl=True, end="\n" )
+        print( df.dtypes )
+        
+    return df
