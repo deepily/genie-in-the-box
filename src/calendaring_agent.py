3ccea87e514fa792c3b69af8708b7e2e40f038a7
deepily <42214371+deepily@users.noreply.github.com>
2023-09-29 13:20:22 -0400

    All references to Langchain, finish implementing first working cut

--- src/calendaring_agent.py
+++ src/calendaring_agent.py
@@ -1,108 +1,67 @@
-import sys
 import os
 import json
 
-# os.environ[ "LANGCHAIN_WANDB_TRACING" ] = "true"
-# # wandb documentation to configure wandb using env variables
-# # https://docs.wandb.ai/guides/track/advanced/environment-variables
-# # here we are configuring the wandb project name
-# os.environ[ "WANDB_PROJECT" ] = "langchain-dataframe-agent"
-
-# path = "/var/genie-in-the-box/src/lib"
-# if path not in sys.path:
-#     sys.path.append( path )
-# else:
-#     print( f"[{path}] already in sys.path" )
-#
-# print( sys.path )
-
-import lib.util as du
-import lib.util_pandas as dup
-
-# path = "/var/genie-in-the-box/src"
-# du.add_to_path( path )
-
+import lib.util           as du
+import lib.util_pandas    as dup
+import lib.util_langchain as ulc
 import lib.util_stopwatch as sw
-# import util_langchain as ulc
-import genie_client as gc
-
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from langchain.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
-
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-import langchain
 
 import pandas as pd
+import openai
+import tiktoken
+
+GPT_4   = "gpt-4-0613"
+GPT_3_5 = "gpt-3.5-turbo-0613"
 
 class CalendaringAgent:
     
     def __init__( self, path_to_df, debug=False, verbose=False ):
         
-        self.langchain         = langchain
-        self.path_to_df        = du.get_project_root() + path_to_df
-        self.debug             = debug
-        self.langchain.debug   = debug
-        self.langchain.verbose = verbose
-        self.df                = pd.read_csv( self.path_to_df )
-        self.df                = dup.cast_to_datetime( self.df )
+        self.debug      = debug
+        self.verbose    = verbose
+        self.path_to_df = du.get_project_root() + path_to_df
         
-        self.llm_4  = ChatOpenAI( model_name=gc.GPT_4, temperature=0.0 )
-        # memory = ConversationBufferMemory( memory_key="chat_history", return_messages=True, verbose=verbose )
+        self.df = pd.read_csv( self.path_to_df )
+        self.df = dup.cast_to_datetime( self.df )
         
-        self.df_agent = create_pandas_dataframe_agent( self.llm_4, self.df, verbose=verbose )
-        template      = self._get_pandas_prompt_template()
-        print( template )
+        self.pandas_system_prompt = self._get_pandas_system_prompt()
         
-        prompt = PromptTemplate(
-            template=template,
-            input_variables=[ "question" ]
-        )
-        self.llm_chain = LLMChain( prompt=prompt, llm=self.llm_4 )
-        # self.df_agent.memory = memory
+        self.question         = None
+        self.response_dict    = None
+        self.formatted_output = None
+    
+    
+    def get_token_count( self, to_be_tokenized, model=GPT_4 ):
         
-    def run_prompt( self, question ):
+        encoding   = tiktoken.encoding_for_model( model )
+        num_tokens = len( encoding.encode( to_be_tokenized ) )
         
-        timer = sw.Stopwatch( "Running pandas prompt..." )
-        question_plus_coda = self._get_question_plus_coda( question )
-        self.response      = self.llm_chain.run( question=question_plus_coda )
-        timer.print( "Done!" )
+        return num_tokens
+    
+    def _get_pandas_system_prompt( self ):
         
-        return self.response
+        csv = self.df.head( 3 ).to_csv( header=True, index=False )
+        csv = csv + self.df.tail( 3 ).to_csv( header=False, index=False )
         
-    def _get_pandas_prompt_template( self, question=None):
-        
-        dtypes = self.df.dtypes
-        csv    = self.df.head( 2 ).to_csv( header=True, index=False)
-        csv    = csv + self.df.tail( 2 ).to_csv( header=False, index=False )
-        
-        pandas_prompt_template = f"""
-        You are working with a pandas dataframe in Python. The name of the dataframe is `df`.
+        pandas_system_prompt = f"""
+        You are an expert software engineer working with a pandas dataframe in Python containing calendaring and events information. The name of the dataframe is `df`.
 
         This is the ouput from `print(df.head().to_csv())`, in CSV format:
 
         {csv}
-        
-        This is the output from `print(df.dtypes)`:
 
-        {dtypes}
+        This is the output from `print(self.df.event_type.value_counts())`:
 
-        As you generate the python code needed to answer the question asked of you below, I want you to:
+        {self.df.event_type.value_counts()}
 
-        1) Question: Ask yourself if you understand the question that I am asking you
+        As you generate the python code needed to answer the events and calendaring question asked of you below, I want you to:
+
+        1) Question: Ask yourself if you understand the question that I am asking you.  Pay attention to the details!
         2) Think: Before you do anything, think out loud about what I'm asking you to do, including what are the steps that you will need to take to solve this problem. Be critical of your thought process!
-        3) Code: Generate a verbatim list of code that you used to arrive at your answer, one line of code per item on the list. The code must be complete, syntactically correct, and capable of runnning to completion. The last line of your code must be the variable `solution`, which represents the answer.
+        3) Code: Generate a verbatim list of code that you used to arrive at your answer, one line of code per item on the list. The code must be complete, syntactically correct, and capable of runnning to completion. The last line of your code must be the variable `solution`, which represents the answer. Make sure that any filtering you perform matches the question asked of you by the user!
         4) Return: Report on the object type of the variable `solution` in your last line of code. Use one word to represent the object type.
         5) Explain: Briefly and succinctly explain your code in plain English.
-        
+
         Format: return your response as a JSON object in the following fields:
         {{
             "question": "The question, verbatim and without modification, that your code attempts to answer",
@@ -112,26 +71,157 @@ class CalendaringAgent:
             "explanation": "A brief explanation of your code",
         }}
 
-        Question: {question}
-        """
-        return pandas_prompt_template
-    
-    def _get_question_plus_coda( self, event_query ):
-        
-        question_plus_coda_template = f"""
-        {event_query}
-
         Hint: An event that I have today may have started before today and may end tomorrow or next week, so be careful how you filter on dates.
         Hint: When filtering by dates, use `pd.Timestamp( day )` to convert a Python datetime object into a Pandas `datetime64[ns]` value.
         Hint: If your solution variable is a dataframe, it should include all columns in the dataframe.
-        
-        Begin!
+
+        Wait until you're presented with the question to begin.
         """
-        return question_plus_coda_template
+        
+        return pandas_system_prompt
     
+    def _query_gpt( self, preamble, query, model=GPT_4, debug=False ):
+        
+        openai.api_key = os.getenv( "FALSE_POSITIVE_API_KEY" )
+        
+        if debug:
+            timer = sw.Stopwatch( msg=f"Asking ChatGPT [{model}]...".format( model ) )
+        
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                { "role": "system", "content": preamble },
+                { "role": "user", "content": query }
+            ],
+            temperature=0,
+            max_tokens=2000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        if debug:
+            timer.print( use_millis=True )
+            if self.verbose:
+                print( json.dumps( response, indent=4 ) )
+        
+        return response[ "choices" ][ 0 ][ "message" ][ "content" ].strip()
     
-# Add main method
+    def run_prompt( self, question ):
+        
+        prompt_model = GPT_4
+        if self.debug:
+            
+            count = self.get_token_count( self.pandas_system_prompt, model=prompt_model )
+            if self.verbose:
+                du.print_banner( f"Token count for pandas_system_prompt: [{count}]", prepend_nl=True )
+                print( self.pandas_system_prompt )
+            else:
+                print( "Token count for pandas_system_prompt: [{}]".format( count ) )
+            
+        self.question      = question
+        self.response      = self._query_gpt( self.pandas_system_prompt, self.question, model=prompt_model, debug=self.debug )
+        self.response_dict = json.loads( self.response )
+        
+        if self.debug and self.verbose: print( json.dumps( self.response_dict, indent=4 ) )
+        
+        return self.response_dict
+    
+    def run_code( self ):
+        
+        self.code_response = ulc.assemble_and_run_solution(
+            self.response_dict[ "code" ], path="/src/conf/long-term-memory/events.csv",
+            solution_code_returns=self.response_dict[ "return" ], debug=False
+        )
+        if self.debug and self.verbose:
+            du.print_banner( "Code output", prepend_nl=True )
+            for line in self.code_response[ "output" ].split( "\n" ):
+                print( line )
+        
+        return self.code_response
+    
+    def format_output( self ):
+        
+        format_model = GPT_3_5
+        preamble     = self.get_formatting_preamble()
+        instructions = self.get_formatting_instructions()
+        
+        if self.debug:
+            
+            # preamble
+            count = self.get_token_count( preamble, model=format_model )
+            if self.verbose:
+                du.print_banner( f"Token count for preamble: [{count}]", prepend_nl=True )
+                print( preamble )
+            else:
+                print( "Token count for preamble: [{}]".format( count ) )
+            
+            # instructions
+            count = self.get_token_count( instructions, model=format_model )
+            if self.verbose:
+                du.print_banner( f"Token count for instructions: [{count}]", prepend_nl=True )
+                print( instructions )
+            else:
+                print( "Token count for instructions: [{}]".format( count ) )
+            
+        self.formatted_output = self._query_gpt( preamble, instructions, model=format_model, debug=self.debug )
+        
+        return self.formatted_output
+    
+    def get_formatting_preamble( self ):
+        
+        rows = self.code_response[ "output" ].split( "\n" )
+        row_count = len( rows )
+        
+        lines = [ ]
+        line_number = 1
+        
+        for row in rows:
+            lines.append( f"{line_number}) {row}" )
+            line_number += 1
+        
+        lines = "\n".join( lines )
+        
+        preamble = f"""
+        You are an expert in converting raw data into conversational English.
+
+        The following {row_count} rows of JSONL formatted data are the output from a query on a pandas dataframe about events on my calendar.
+        
+        The query was: `{self.question}`
+
+        JSONL output:
+
+        {lines}
+        """
+        return preamble
+    
+    def get_formatting_instructions( self ):
+        
+        instructions = f"""
+        Reformat and rephrase the JSONL data that I just showed you in conversational English so that it answers this question: `{self.question}`
+
+        Use this format: "You have a two hour lunch date with your friend Bob at noon today at Burgerland.
+
+        Each line of the output that you create should contain one event."
+        """
+        return instructions
+    
 if __name__ == "__main__":
     
-    agent    = CalendaringAgent( path_to_df="/src/conf/long-term-memory/events.csv", debug=True )
-    response = agent.run_prompt( "What concerts do I have today?" )
+    agent = CalendaringAgent( path_to_df="/src/conf/long-term-memory/events.csv", debug=True, verbose=True )
+    
+    # question         = "What todo items do I have on my calendar for this week?"
+    # question         = "What todo items do I have on my calendar for today?"
+    # question         = "Do I have any birthdays on my calendar this week?"
+    question         = "When is Juan's birthday?"
+    timer            = sw.Stopwatch( msg=f"Processing [{question}]..." )
+    response_dict    = agent.run_prompt( question )
+    code_response    = agent.run_code()
+    formatted_output = agent.format_output()
+    
+    timer.print( use_millis=True )
+    
+    du.print_banner( question, prepend_nl=True )
+    for line in formatted_output.split( "\n" ):
+        print( line )
+        
+    
