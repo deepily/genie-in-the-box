ef688ffc1593e32e5b148c29d295887f26a8cc3e
deepily <42214371+deepily@users.noreply.github.com>
2023-10-12 23:37:10 -0400

    Tweak code in run_code

--- src/lib/agents/runnable.py
+++ src/lib/agents/runnable.py
@@ -6,10 +6,14 @@ class Runnable:
         self.debug                = debug
         self.verbose              = verbose
         
+        self.code                 = None
+        
         self.prompt_response      = None
         self.prompt_response_dict = None
         
         self.code_response_dict   = None
+        self.answer               = None
+        # self.answer_conversational= None
         self.error                = None
 
     def run_code(self):
@@ -18,8 +22,10 @@ class Runnable:
             self.prompt_response_dict[ "code" ], path="/src/conf/long-term-memory/events.csv",
             solution_code_returns=self.prompt_response_dict.get( "returns", "string" ), debug=self.debug
         )
+        self.answer = self.code_response_dict[ "output" ]
+        
         if self.debug and self.verbose:
-            du.print_banner("Code output", prepend_nl=True)
+            du.print_banner("Code output", prepend_nl=True )
             for line in self.code_response_dict[ "output" ].split( "\n" ):
                 print( line )
 
