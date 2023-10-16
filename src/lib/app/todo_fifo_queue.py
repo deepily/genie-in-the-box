e3829696d175752ad95d8fcdb341f77f76d129bb
deepily <42214371+deepily@users.noreply.github.com>
2023-10-16 16:45:43 -0400

    Move flask methods for managing the TODOQ into this external module

---
+++ src/lib/app/todo_fifo_queue.py
@@ -0,0 +1,73 @@
+from flask import url_for
+from lib.app.fifo_queue import FifoQueue
+from lib.agents.agent_calendaring import CalendaringAgent
+from lib.memory.solution_snapshot_mgr import SolutionSnapshotManager
+from lib.utils.util import print_banner, get_current_datetime
+from lib.memory.solution_snapshot import SolutionSnapshot
+
+
+class TodoFifoQueue( FifoQueue ):
+    def __init__( self, socketio, snapshot_mgr, app ):
+        
+        super().__init__()
+        
+        self.socketio = socketio
+        self.snapshot_mgr = snapshot_mgr
+        self.app = app
+        self.push_count = 0
+    
+    def push_job( self, question ):
+        
+        self.push_count += 1
+        
+        print_banner( f"Question: [{question}]", prepend_nl=True )
+        similar_snapshots = self.snapshot_mgr.get_snapshots_by_question( question, threshold=90.0 )
+        print()
+        
+        if len( similar_snapshots ) > 0:
+            best_snapshot = similar_snapshots[ 0 ][ 1 ]
+            best_score = similar_snapshots[ 0 ][ 0 ]
+            
+            lines_of_code = best_snapshot.code
+            if len( lines_of_code ) > 0:
+                print_banner( f"Code for [{best_snapshot.question}]:" )
+            else:
+                print_banner( "Code: NONE found?" )
+            for line in lines_of_code:
+                print( line )
+            if len( lines_of_code ) > 0:
+                print()
+            
+            job = best_snapshot.get_copy()
+            print( "Python object ID for copied job: " + str( id( job ) ) )
+            job.add_synonymous_question( question, best_score )
+            
+            job.run_date = get_current_datetime()
+            job.push_counter = self.push_count
+            job.id_hash = SolutionSnapshot.generate_id_hash( job.push_counter, job.run_date )
+            
+            print()
+            
+            if self.size() != 0:
+                suffix = "s" if self.size() > 1 else ""
+                with self.app.app_context():
+                    url = url_for( 'get_tts_audio' ) + f"?tts_text={self.size()} job{suffix} before this one"
+                print( f"Emitting TODO url [{url}]..." )
+                self.socketio.emit( 'audio_update', { 'audioURL': url } )
+            else:
+                print( "No jobs ahead of this one in the todo Q" )
+            
+            self.push( job )
+            self.socketio.emit( 'todo_update', { 'value': self.size() } )
+            
+            return f'Job added to queue. Queue size [{self.size()}]'
+        
+        else:
+            calendaring_agent = CalendaringAgent( self.app.EVENTS_DF_PATH, question=question,
+                                                  push_counter=self.push_count, debug=True, verbose=True
+                                                  )
+            self.push( calendaring_agent )
+            self.socketio.emit( 'audio_update', { 'audioURL': url_for( 'get_tts_audio' ) + "?tts_text=Working on it!" } )
+            self.socketio.emit( 'todo_update', { 'value': self.size() } )
+            
+            return f'No similar snapshots found, adding NEW CalendaringAgent to TODO queue. Queue size [{self.size()}]'
