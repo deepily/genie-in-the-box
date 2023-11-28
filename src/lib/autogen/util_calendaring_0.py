6fee145c39b5f9ab99d64c35c0c9ddcf022bb27f
deepily <42214371+deepily@users.noreply.github.com>
2023-11-28 12:43:38 -0500

    Delete v1 serialized solutions

--- src/lib/autogen/util_calendaring_0.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from datetime import datetime
-def get_todays_events_by_type(df, **kwargs):
-    event_type = kwargs.get('event_type')
-    today = pd.Timestamp(datetime.now()).normalize()
-    events_today = df[(df['event_type'] == event_type) & (df['start_date'] <= today) & (df['end_date'] >= today)]
-    return events_today.to_json(orient='records', lines=True)
--- src/lib/autogen/util_calendaring_2.py
+++
@@ -1,4 +0,0 @@
-def get_birthday_by_name(df, **kwargs):
-    name = kwargs.get('name')
-    filtered_df = df[(df['event_type'] == 'birthday') & (df['name'] == name)]
-    return filtered_df['start_date'].values[0] if not filtered_df.empty else f'No birthday found for {name}'
