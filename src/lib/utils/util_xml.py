b2c44713689bca53861520adf04cf8b02885d11f
deepily <42214371+deepily@users.noreply.github.com>
2023-12-06 15:46:46 -0500

    KLUDGE: Add rescue_code_using_tick_tick_tick_syntax

--- src/lib/utils/util_xml.py
+++ src/lib/utils/util_xml.py
@@ -1,4 +1,7 @@
 import re
+
+import lib.utils.util as du
+
 def get_value_by_xml_tag_name( xml_string, name, default_value=f"Error: `{{name}}` not found in xml_string" ):
     
     if f"<{name}>" not in xml_string or f"</{name}>" not in xml_string:
@@ -35,3 +38,40 @@ def get_code_list( xml_string, debug=False ):
                 if debug: print( "[]" )
         
     return code_list
+
+
+def rescue_code_using_tick_tick_tick_syntax( raw_response_text, debug=True ):
+    
+    if debug: print( f"before: [{raw_response_text}]" )
+    raw_response_text = raw_response_text.strip()
+    if debug: print( f"after: [{raw_response_text}]" )
+    
+    if raw_response_text.startswith( "```python" ) and raw_response_text.endswith( "```" ):
+        
+        msg = "¡Yay! Returning rescued code list using default tick tick tick syntax"
+        if debug:
+            du.print_banner( msg )
+        else:
+            print( msg )
+        
+        lines = raw_response_text.split( "```python" )[ 1 ]
+        lines = lines.split( "```" )[ 0 ]
+        lines = lines.split( "\n" )
+        
+        # wrap each line with a xml-esque line tag
+        lines = [ f"<line>{line}</line>" for line in lines ]
+        lines = "\n".join( lines )
+        
+        if debug:
+            for line in lines.split( "\n" ): print( line )
+        
+        return lines
+    
+    else:
+        
+        if debug:
+            du.print_banner( "¡Boo!, no ```python found, either!", expletive=True )
+        else:
+            print( "¡Boo!, no ```python found, either!" )
+            
+        return ""
