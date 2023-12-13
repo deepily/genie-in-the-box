869c9d343e382ce38d35c6e48d7b7cb83ed3e395
deepily <42214371+deepily@users.noreply.github.com>
2023-12-12 22:51:52 -0500

    move to version 2.0 and swap in better model

--- src/scripts/run-flask-tts.sh
+++ src/scripts/run-flask-tts.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# cd /var/genie-in-the-box/src
+# used before version two was released
+# python3 TTS/server/server.py --model_name tts_models/en/ljspeech/tacotron2-DCA --vocoder_name vocoder_models/en/ljspeech/multiband-melgan --use_cuda True
 
-# python3 TTS/server/server.py --use_cuda True
-python3 TTS/server/server.py --model_name tts_models/en/ljspeech/tacotron2-DCA --vocoder_name vocoder_models/en/ljspeech/multiband-melgan --use_cuda True
+python3 TTS/server/server.py --model_name tts_models/en/ljspeech/tacotron2-DDC --vocoder_name vocoder_models/en/ljspeech/hifigan_v2 --use_cuda True
