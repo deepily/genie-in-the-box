dc85f549e4d73bdeab527834520fa377e9dd8a62
deepily <42214371+deepily@users.noreply.github.com>
2023-12-14 18:08:22 -0500

    add cli-esque startup name=value pairs

--- src/scripts/run-flask-gib.sh
+++ src/scripts/run-flask-gib.sh
@@ -3,5 +3,6 @@
 cd /var/genie-in-the-box/src
 
 export FLASK_DEBUG=1
+export GIB_FLASK_CLI_ARGS="config_path=/src/conf/gib-app.ini splainer_path=/src/conf/gib-app-splainer.ini config_block_id=Genie+in+the+Box:+Development"
 
 flask run --port 7999 --host 0.0.0.0
