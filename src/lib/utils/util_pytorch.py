80639bd4d91bec4669aa7618ebde17785945ba9f
deepily <42214371+deepily@users.noreply.github.com>
2024-01-19 09:33:28 -0500

    Add test for CPU allocation

--- src/lib/utils/util_pytorch.py
+++ src/lib/utils/util_pytorch.py
@@ -2,3 +2,11 @@ def print_device_allocation( model ):
 
     for name, param in model.named_parameters():
        print( f"{name}: {param.device}" )
+
+def is_allocated_to_cpu( model ):
+
+    # test to see if *any* parameter is stashed on the cpu
+    for name, param in model.named_parameters():
+        if param.device.type == "cpu": return True
+
+    return False
