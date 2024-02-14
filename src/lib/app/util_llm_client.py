6cba9007646c1f38afa41ed86799e37fbc167a67
deepily <42214371+deepily@users.noreply.github.com>
2024-02-13 22:42:37 -0500

    Tweak tokens per second calculation for in memory inference

--- src/lib/app/util_llm_client.py
+++ src/lib/app/util_llm_client.py
@@ -19,16 +19,15 @@ def query_llm_in_memory( model, tokenizer, prompt, device="cuda:0", model_name="
     
     # Skip decoding the prompt part of the output
     input_length = inputs[ "input_ids" ].size( 1 )
-    raw_output   = tokenizer.decode( generation_output[ 0 ][ input_length: ] )
+    raw_response   = tokenizer.decode( generation_output[ 0 ][ input_length: ] )
     
     timer.print( msg="Done!", use_millis=True, end="\n" )
-    tokens_per_second = len( raw_output ) / ( timer.get_delta_ms() / 1000.0 )
-    print( f"Tokens per second [{round( tokens_per_second, 1 )}]" )
-    
-    # response   = raw_output.split( "### Response:" )[ 1 ]
+    seconds = timer.get_delta_ms() / 1000.0
+    tokens_per_second = len( generation_output[ 0 ][ input_length: ] ) / seconds
+    print( f"Tokens per second [{round( tokens_per_second, 1 )}] input tokens [{input_length}] + xml response tokens [{len( generation_output[ 0 ][ input_length: ] )}] = total tokens i/o [{len( generation_output[ 0 ] )}]" )
     
     # Remove the <s> and </s> tags
-    response = raw_output.replace( "</s><s>", "" ).strip()
+    response = raw_response.replace( "</s><s>", "" ).strip()
     # Remove white space outside XML tags
     response = re.sub( r'>\s+<', '><', response )
     
