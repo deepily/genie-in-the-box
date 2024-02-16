805b233b1ed74f1d53ad8690adcad3a79628c473
deepily <42214371+deepily@users.noreply.github.com>
2024-02-16 16:38:17 -0500

    Preliminary usage of routing agent

--- src/lib/app/todo_fifo_queue.py
+++ src/lib/app/todo_fifo_queue.py
@@ -15,16 +15,15 @@ from ephemera.prompts.xml_fine_tuning_prompt_generator import XmlFineTuningPromp
 
 
 class TodoFifoQueue( FifoQueue ):
-    def __init__( self, socketio, snapshot_mgr, app, path_to_events_df, config_mgr ):
+    def __init__( self, socketio, snapshot_mgr, app, config_mgr=None ):
         
         super().__init__()
         
-        self.socketio = socketio
+        self.socketio     = socketio
         self.snapshot_mgr = snapshot_mgr
-        self.app = app
+        self.app          = app
         self.push_counter = 0
-        self.path_to_events_df = path_to_events_df
-        self.config_mgr = config_mgr
+        self.config_mgr   = config_mgr
         
         # Set by set_llm() below
         self.cmd_llm_in_memory = None
@@ -87,38 +86,13 @@ class TodoFifoQueue( FifoQueue ):
         else:
             
             self.socketio.emit( 'notification_sound_update', { 'soundFile': '/static/gentle-gong.mp3' } )
-            print( "No similar snapshots found" )
-            router_prompt_template = du.get_file_as_string( du.get_project_root() + self.config_mgr.get( "agent_router_prompt_path_wo_root" ) )
+            print( "No similar snapshots found, calling routing LLM..." )
             
-            # xml_ftp_generator      = XmlFineTuningPromptGenerator( tgi_url=self.config_mgr.get( "tgi_server_router_url" ), debug=False, silent=True, init_prompt_templates=False )
-            # # TODO: The router is loaded into memory, and not served via TGI. Fix this!
-            # response               = xml_ftp_generator.query_llm_tgi( router_prompt, model_name=self.config_mgr.get( "tgi_server_router_name" ), max_new_tokens=1024, temperature=0.25, top_k=10, top_p=0.9, silent=False )
-            # print( response )
-            # response = dux.strip_all_white_space( response )
-            # print( response )
-            # command = dux.get_value_by_xml_tag_name( response, "command" )
-            
-            # print( self.config_mgr.get( "vox_command_llm_device_map", default="cuda:0" ) )
-            # print( self.config_mgr.get( "vox_command_llm_name" ) )
-            # print( self.cmd_llm_in_memory )
-            # print( self.cmd_llm_tokenizer )
-            
-            response = llm_client.query_llm_in_memory(
-                self.cmd_llm_in_memory,
-                self.cmd_llm_tokenizer,
-                router_prompt_template.format( voice_command=question ),
-                model_name=self.config_mgr.get( "vox_command_llm_name" ),
-                device=self.config_mgr.get( "vox_command_llm_device_map", default="cuda:0" )
-            )
-            print( f"LLM response: [{response}]" )
-            # Parse results
-            command = dux.get_value_by_xml_tag_name( response, "command" )
+            command, args = self._get_routing_command( question )
             
             if command == "none":
-                
                 msg = "Hmm... I'm not certain what to do with that question, Could you rephrase and try again?"
             else:
-                
                 msg = "TO DO: Implement command " + command
             
             with self.app.app_context():
@@ -128,14 +102,32 @@ class TodoFifoQueue( FifoQueue ):
             
             return msg
             
-            # calendaring_agent = IncrementalCalendaringAgent( self.path_to_events_df, question=question, push_counter=self.push_counter, debug=True, verbose=False )
+            # calendaring_agent = IncrementalCalendaringAgent( question=question, push_counter=self.push_counter, debug=True, verbose=False )
             # self.push( calendaring_agent )
             # msg = f"No similar snapshots found, adding NEW IncrementalCalendaringAgent to TODO queue. Queue size [{self.size()}]"
             # print( msg )
             # return msg
             #
-            # agent = FunctionMappingAgent( "/src/conf/long-term-memory/events.csv", question=question, push_counter=self.push_counter, debug=True, verbose=True )
+            # agent = FunctionMappingAgent( question=question, push_counter=self.push_counter, debug=True, verbose=True )
             # self.push( agent )
             # self.socketio.emit( 'todo_update', { 'value': self.size() } )
             #
             # return f'No similar snapshots found, adding NEW FunctionMappingAgent to TODO queue. Queue size [{self.size()}]'
+
+    def _get_routing_command( self, question ):
+        
+        router_prompt_template = du.get_file_as_string( du.get_project_root() + self.config_mgr.get( "agent_router_prompt_path_wo_root" ) )
+        
+        response = llm_client.query_llm_in_memory(
+            self.cmd_llm_in_memory,
+            self.cmd_llm_tokenizer,
+            router_prompt_template.format( voice_command=question ),
+            model_name=self.config_mgr.get( "vox_command_llm_name" ),
+            device=self.config_mgr.get( "vox_command_llm_device_map", default="cuda:0" )
+        )
+        print( f"LLM response: [{response}]" )
+        # Parse results
+        command = dux.get_value_by_xml_tag_name( response, "command" )
+        args    = dux.get_value_by_xml_tag_name( response, "args" )
+        
+        return command, args
