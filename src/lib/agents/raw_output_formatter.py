baa7c650996250659ce1353faaba25b72c7a41b1
deepily <42214371+deepily@users.noreply.github.com>
2024-02-20 22:32:26 -0500

    Add raw XML birthdays string for manual test case

--- src/lib/agents/raw_output_formatter.py
+++ src/lib/agents/raw_output_formatter.py
@@ -46,7 +46,8 @@ class RawOutputFormatter:
     
 if __name__ == "__main__":
     
-    routing_command = "agent router go to date and time"
+    # routing_command = "agent router go to date and time"
+    routing_command = "agent router go to calendar"
     # question        = "What time is it now?"
     # question        = "What day is today?"
     # question        = "Is it daytime or nighttime?"
@@ -55,7 +56,41 @@ if __name__ == "__main__":
     # question        = "Is today Tuesday or Wednesday?"
     # question        = "Is it time to go to bed yet?"
     # question        = "What time is it now?"
-    question        = "Is it daylight savings time?"
-    raw_output      = "8:57PM EDT, Monday, February 19, 2024"
+    # question        = "Is it daylight savings time?"
+    # raw_output      = "8:57PM EDT, Monday, February 19, 2024"
+    
+    question        = "Who has birthdays this week?"
+    raw_output      = """
+    <data>
+  <row>
+    <index>57</index>
+    <start_date>2024-02-20 00:00:00</start_date>
+    <end_date>2024-02-20 00:00:00</end_date>
+    <start_time>00:00</start_time>
+    <end_time>23:59</end_time>
+    <event_type>birthday</event_type>
+    <recurrent>True</recurrent>
+    <recurrence_interval>2 month</recurrence_interval>
+    <priority_level>medium</priority_level>
+    <name>Tom Ruiz</name>
+    <relationship>brother</relationship>
+    <description_who_what_where>Tom Ruiz's birthday party at their favorite bar</description_who_what_where>
+  </row>
+  <row>
+    <index>71</index>
+    <start_date>2024-02-23 00:00:00</start_date>
+    <end_date>2024-02-23 00:00:00</end_date>
+    <start_time>00:00</start_time>
+    <end_time>23:59</end_time>
+    <event_type>birthday</event_type>
+    <recurrent>True</recurrent>
+    <recurrence_interval>4 day</recurrence_interval>
+    <priority_level>high</priority_level>
+    <name>Alice</name>
+    <relationship>aunt</relationship>
+    <description_who_what_where>Alice's birthday party at their favorite bar</description_who_what_where>
+  </row>
+</data>
+    """
     formatter = RawOutputFormatter( question, raw_output, routing_command, debug=False, verbose=False)
     print( formatter.format_output() )
