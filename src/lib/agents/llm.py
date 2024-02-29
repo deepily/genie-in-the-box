628491fecb8af6e46421c04fea213b5babb8ecbc
deepily <42214371+deepily@users.noreply.github.com>
2024-02-29 17:00:04 -0500

    Clean up code, add prompt splitting logic based on new and old prompt instruction formatting found here: https://huggingface.co/Phind/Phind-CodeLlama-34B-v2

--- src/lib/agents/llm.py
+++ src/lib/agents/llm.py
@@ -15,42 +15,65 @@ class Llm:
     GPT_3_5      = "gpt-3.5-turbo-1106"
     PHIND_34B_v2 = "Phind/Phind-CodeLlama-34B-v2"
 
-    def __init__( self, model, prompt, default_url=None, debug=False, verbose=False ):
+    def __init__( self, model=PHIND_34B_v2, default_url=None, debug=False, verbose=False ):
         
         self.debug          = debug
         self.verbose        = verbose
         self.model          = model
-        self.prompt         = prompt
         # Get the TGI server URL for this context
         self.tgi_server_url = du.get_tgi_server_url_for_this_context( default_url=default_url )
 
-    def query_llm( self, max_new_tokens=1024, temperature=0.5, top_k=100, top_p=0.25, debug=True ):
+    def query_llm( self, model=None, prompt=None, preamble=None, question=None, max_new_tokens=1024, temperature=0.5, top_k=100, top_p=0.25, debug=False, verbose=False ):
+        
+        if preamble is None and question is None and prompt is None:
+            raise ValueError( "ERROR: Neither prompt, preamble, nor question has a value set!" )
+        
+        # Allow us to override the prompt, preamble, and question set when instantiated
+        if model    is not None: self.model    = model
         
         if self.model == Llm.PHIND_34B_v2:
             
-            # self.debug = debug
+            # Quick sanity check
+            if prompt is None: raise ValueError( "ERROR: Prompt is `None`!" )
+            
             return self._query_llm_phind(
-                self.prompt, max_new_tokens=max_new_tokens, temperature=temperature, top_k=top_k, top_p=top_p, debug=debug
+                prompt, max_new_tokens=max_new_tokens, temperature=temperature, top_k=top_k, top_p=top_p, debug=debug, verbose=verbose
             )
         
         else:
-            # KLUDGE: Test for divisibility
-            if "### Input:" not in self.prompt:
-                print( "(KLUDGE) ERROR: Prompt isn't divisible, no '### Input:' found in prompt!" )
-                print( f"Prompt: [{self.prompt}]" )
-                raise ValueError( "ERROR: No '### Input:' found in prompt!" )
-            
-            preamble = self.prompt.split( "### Input:" )[ 0 ]
-            question = self.prompt.split( "### Input:" )[ 1 ]
-            question = "### Input:" + question
-            
-            if debug:
-                print( f"Preamble: [{preamble}]" )
-                print( f"Question: [{question}]" )
+            # Test for divisibility using an "all in one" prompt
+            if prompt is not None:
                 
-            return self._query_llm_openai( preamble, question, debug=debug )
+                input_present        = "### Input:" in prompt
+                user_message_present = "### User Message:" in prompt
+                
+                if not input_present and not user_message_present:
+                    msg = "ERROR: Prompt isn't divisible, '### Input:' and '### User Message:' not found in prompt!"
+                    print( msg )
+                    print( f"Prompt: [{prompt}]" )
+                    raise ValueError( msg )
+                
+                if input_present:
+                    preamble = prompt.split( "### Input:" )[ 0 ]
+                    question = prompt.split( "### Input:" )[ 1 ]
+                
+                if user_message_present:
+                    preamble = prompt.split( "### User Message:" )[ 0 ]
+                    question = prompt.split( "### User Message:" )[ 1 ]
+                    
+                # Strip out system prompt
+                preamble = preamble.replace( "### System Prompt", "" )
+                
+                if debug:
+                    print( f"Preamble: [{preamble}]" )
+                    print( f"Question: [{question}]" )
+                
+            elif preamble is None or question is None:
+                raise ValueError( "ERROR: Preamble or question is `None`!" )
+                
+            return self._query_llm_openai( preamble, question, debug=debug, verbose=verbose )
     
-    def _query_llm_openai( self, preamble, query, debug=False ):
+    def _query_llm_openai( self, preamble, query, debug=False, verbose=False ):
         
         openai.api_key = os.getenv( "FALSE_POSITIVE_API_KEY" )
         
@@ -77,7 +100,7 @@ class Llm:
         return response.choices[ 0 ].message.content.strip()
     
     def _query_llm_phind(
-            self, prompt, max_new_tokens=1024, temperature=0.25, top_k=10, top_p=0.9, debug=False
+            self, prompt, max_new_tokens=1024, temperature=0.25, top_k=10, top_p=0.9, debug=False, verbose=False
     ):
         timer = Stopwatch( msg=f"Asking LLM [{self.model}]...".format( self.model ) )
         
@@ -117,3 +140,17 @@ class Llm:
                     print( line )
         
         return response
+    
+    
+# Add main method
+if __name__ == "__main__":
+    
+    llm = Llm( model=Llm.PHIND_34B_v2, debug=True, verbose=True )
+    
+    prompt_template = du.get_file_as_string( du.get_project_root() + "/src/conf/prompts/incremental-agents/events/calendaring.txt" )
+    prompt = prompt_template.format( question="Do I have any concerts this week?" )
+    # print( prompt)
+    response = llm.query_llm( prompt=prompt, debug=False, verbose=False )
+
+    # du.print_banner( "Response")
+    # print( response )
