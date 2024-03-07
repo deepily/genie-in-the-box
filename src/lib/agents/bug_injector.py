a6a8155193bcb9c23b279e9bc16b9a1dc95a8161
deepily <42214371+deepily@users.noreply.github.com>
2024-03-07 12:37:13 -0500

    Reflector to inherent from agent base object

--- src/lib/agents/bug_injector.py
+++ src/lib/agents/bug_injector.py
@@ -5,107 +5,66 @@ from lib.agents.agent_base import AgentBase
 from lib.agents.llm   import Llm
 
 class BugInjector( AgentBase ):
-    def __init__( self, code, debug=True, verbose=True ):
+    def __init__( self, code, example="", debug=True, verbose=True ):
         
-        super().__init__( debug=debug, verbose=verbose )
+        super().__init__( debug=debug, verbose=verbose, routing_command="agent router go to bug injector" )
         
-        self.code                 = code
-        self.prompt_components    = None
-        self.prompt_response_dict = None
-    
-    def _initialize_prompt_components( self ):
-        
-        code_with_line_numbers = du.get_source_code_with_line_numbers( self.code.copy(), join_str="\n" )
-        
-        prompt = f"""
-        You are a Python or Pandas code bug injector.
-        
-        Please introduce one, and only one, Python or Pandas syntax or spelling error inside the function body of the provided source code snippet below.
-        
-        Source code:
-{code_with_line_numbers}
-        
-        Remember: The error should prevent the program from running.
-        
-        You must only return only one line of source code that you have modified to fail at runtime
-        """
-        
-        instructions = """
-        Format the response as follows, replacing the placeholders with the actual details:
-        <response>
-            <line-number>[line number where bug is introduced]</line-number>
-            <bug>[one line of modified source code with bug in it]</bug>
-        </response>
-        """
-        if self.debug: print( f"prompt: [{prompt}]" )
-        
-        self.prompt_components = {
-                "preamble": prompt,
-            "instructions": instructions
+        self.prompt_template        = du.get_file_as_string( du.get_project_root() + self.prompt_template_paths[ self.routing_command ] )
+        self.prompt_response_dict   = {
+            "code"   : code,
+            "example": example
         }
+        self.prompt                 = self._get_prompt()
         
-        return self.prompt_components
-    
-    def format_output( self ):
-        pass
-    
-    def is_code_runnable( self ):
-        pass
-    
-    def run_prompt( self, question="" ):
+    def _get_prompt( self ):
         
-        print( "BugInjector.run_prompt() called..." )
+        code_with_line_numbers = du.get_source_code_with_line_numbers( self.prompt_response_dict[ "code" ].copy(), join_str="\n" )
         
-        model = Llm.PHIND_34B_v2  # Asking LLM [TGI/Phind-CodeLlama-34B-v2]... Done! in 1,270 ms
-        # model = Llm.GPT_4       # Asking LLM [gpt-4-0613]... Done! in 5,222 ms
-        self.prompt_components = self._initialize_prompt_components()
-        response               = self._query_llm(
-            self.prompt_components[ "preamble" ], self.prompt_components[ "instructions" ], model=model, temperature=2.0, top_k=1000, top_p=0.25, debug=self.debug
-        )
+        return self.prompt_template.format( code_with_line_numbers=code_with_line_numbers )
+    
+    def run_prompt( self, model_name=None, temperature=0.5, top_p=0.25, top_k=10, max_new_tokens=1024  ):
+        
+        if self.debug: print( "BugInjector.run_prompt() called..." )
+        
+        if model_name is not None: self.model_name = model_name
+        
+        llm = Llm( model=self.model_name, default_url=self.default_url, debug=self.debug, verbose=self.verbose )
+        response = llm.query_llm( prompt=self.prompt, temperature=temperature, top_p=top_p, top_k=top_k, max_new_tokens=max_new_tokens, debug=self.debug, verbose=self.verbose )
         
         line_number = int( dux.get_value_by_xml_tag_name( response, "line-number", default_value="-1" ) )
         bug         =      dux.get_value_by_xml_tag_name( response, "bug",         default_value="" )
         
         if line_number == -1: # or bug == "": # for now, we're going to allow the bug to be an empty string
-            du.print_banner( f"Invalid response from [{model}]", expletive=True )
+            du.print_banner( f"Invalid response from [{model_name}]", expletive=True )
             print( response )
-        elif line_number > len( self.code ):
-            du.print_banner( f"Invalid response from [{model}]", expletive=True )
-            print( f"Line number [{line_number}] out of bounds, code[] length is [{len(self.code)}]" )
+        elif line_number > len( self.prompt_response_dict[ "code" ] ):
+            du.print_banner( f"Invalid response from [{model_name}]", expletive=True )
+            print( f"Line number [{line_number}] out of bounds, code[] length is [{len(self.prompt_response_dict[ 'code' ])}]" )
             print( response )
         elif line_number == 0:
-            du.print_banner( f"Invalid response from [{model}]", expletive=True )
+            du.print_banner( f"Invalid response from [{model_name}]", expletive=True )
             print( f"Line number [{line_number}] is invalid, line numbers SHOULD start at 1" )
             print( response )
         else:
             if self.debug:
                 du.print_banner( "BEFORE: untouched code", prepend_nl=True, end="\n" )
-                du.print_list( self.code )
+                du.print_list( self.prompt_response_dict[ "code" ] )
                 
-            print( f"Bug generated for line_number: [{line_number}], bug: [{bug}]" )
+            if self.debug: print( f"Bug generated for line_number: [{line_number}], bug: [{bug}]" )
             # prepend a blank line to the code, so that the line numbers align with the line numbers in the prompt
-            self.code = [ "" ] + self.code
+            self.prompt_response_dict[ "code" ] = [ "" ] + self.prompt_response_dict[ "code" ]
             # todo: do i need to handle possibly mangled preceding white space? if so see: https://chat.openai.com/c/59098430-c164-482e-a82e-01d6c6769978
-            self.code[ line_number ] = bug
+            self.prompt_response_dict[ "code" ][ line_number ] = bug
 
-        self.prompt_response_dict = {
-            "code": self.code,
-        }
         if self.debug:
             du.print_banner( "AFTER: updated code", prepend_nl=True, end="\n" )
-            du.print_list( self.code )
+            du.print_list( self.prompt_response_dict[ "code" ] )
             
         return self.prompt_response_dict
     
-    def _get_user_message( self ):
-        pass
-    
-    def _get_system_message( self ):
-        pass
-    
-    def is_prompt_executable( self ):
-        pass
-
+    def restore_from_serialized_state( file_path ):
+        
+        raise NotImplementedError( "BugInjector.restore_from_serialized_state() not implemented" )
 
 if __name__ == "__main__":
     
@@ -123,8 +82,11 @@ if __name__ == "__main__":
         "print( solution )"
     ]
     
-    bug_injector  = BugInjector( code, debug=False, verbose=False )
-    response_dict = bug_injector.run_prompt()
+    bug_injector  = BugInjector( code, example="solution = get_time()", debug=False, verbose=False )
+    code_response_dict = bug_injector.run_code()
+    bug_injector.print_code( msg="BEFORE: Bug Injection", end="\n" )
     
-    du.print_banner( "BugInjector response_dict", prepend_nl=True, end="\n" )
-    du.print_list( response_dict[ "code" ] )
+    response_dict = bug_injector.run_prompt()
+    bug_injector.run_code()
+    
+    bug_injector.print_code( msg=" AFTER: Bug Injection" )
