cc39aae664c355674cf04c2c1f4bcc482397f7fe
deepily <42214371+deepily@users.noreply.github.com>
2024-03-22 12:15:29 -0400

    Add debugging flags and rename underlying table name

--- src/lib/memory/question_embeddings_table.py
+++ src/lib/memory/question_embeddings_table.py
@@ -1,35 +1,37 @@
 import lib.utils.util as du
 from lib.app.configuration_manager import ConfigurationManager
-from lib.utils.util_stopwatch import Stopwatch
+from lib.utils.util_stopwatch      import Stopwatch
 
 import lancedb
 class QuestionEmbeddingsTable():
     
-    def __init__( self, *args, **kwargs ):
+    def __init__( self, debug=False, verbose=False, *args, **kwargs ):
         
+        self.debug       = debug
+        self.verbose     = verbose
         self._config_mgr = ConfigurationManager( env_var_name="GIB_CONFIG_MGR_CLI_ARGS" )
         
         uri = du.get_project_root() + self._config_mgr.get( "database_path_wo_root" )
         
         db = lancedb.connect( uri )
         
-        self._question_embeddings_tbl = db.open_table( "question_embeddings" )
+        self._question_embeddings_tbl = db.open_table( "question_embeddings_tbl" )
         
         print( f"Opened question_embeddings_tbl w/ [{self._question_embeddings_tbl.count_rows()}] rows" )
         
     def is_in( self, question ):
         
-        timer = Stopwatch( msg=f"is_in( '{question}' )" )
+        if self.debug: timer = Stopwatch( msg=f"is_in( '{question}' )" )
         synonyms = self._question_embeddings_tbl.search().where( f"question = '{question}'" ).limit( 1 ).select( [ "question" ] ).to_list()
-        timer.print( "Done!", use_millis=True )
+        if self.debug: timer.print( "Done!", use_millis=True )
         
         return len( synonyms ) > 0
     
     def get_embedding( self, question ):
         
-        timer = Stopwatch( msg=f"get_embedding( '{question}' )" )
+        if self.debug: timer = Stopwatch( msg=f"get_embedding( '{question}' )" )
         synonyms = self._question_embeddings_tbl.search().where( f"question = '{question}'" ).limit( 1 ).select( [ "embedding" ] ).to_list()
-        timer.print( "Done!", use_millis=True )
+        if self.debug: timer.print( "Done!", use_millis=True )
         
         if not synonyms:
             return None
@@ -40,7 +42,8 @@ class QuestionEmbeddingsTable():
         
         new_row = [ { "question": question, "embedding": embedding } ]
         self._question_embeddings_tbl.add( new_row )
-    # def _init_db( self ):
+    
+    # def _init_tbl( self ):
     #
     #     # question_embeddings_dict = QuestionEmbeddingsDict()
     #     # question_embeddings_dict = dict( question_embeddings_dict )
@@ -58,7 +61,7 @@ class QuestionEmbeddingsTable():
     #
     #     db = lancedb.connect( uri )
     #
-    #     # db.drop_table( "question_embeddings" )
+    #     # db.drop_table( "question_embeddings_tbl" )
     #     # import pyarrow as pa
     #     #
     #     # schema = pa.schema(
@@ -67,14 +70,14 @@ class QuestionEmbeddingsTable():
     #     #         pa.field( "embedding", pa.list_( pa.float32(), 1536 ) )
     #     #     ]
     #     # )
-    #     # self.question_embeddings_tbl = db.create_table( "question_embeddings", schema=schema )
+    #     # self.question_embeddings_tbl = db.create_table( "question_embeddings_tbl", schema=schema )
     #     # self.question_embeddings_tbl.create_fts_index( "question" )
     #     # print( f"New: Table.count_rows: {self.question_embeddings_tbl.count_rows()}" )
     #     # self.question_embeddings_tbl.add( df_dict )
     #     # print( f"New: Table.count_rows: {self.question_embeddings_tbl.count_rows()}" )
     #
     #     print( db.table_names() )
-    #     self.question_embeddings_tbl = db.open_table( "question_embeddings" )
+    #     self.question_embeddings_tbl = db.open_table( "question_embeddings_tbl" )
     #     du.print_banner( "Table:" )
     #     print( self.question_embeddings_tbl.head( 10 ) )
     #
