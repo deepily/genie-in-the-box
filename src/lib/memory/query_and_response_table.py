3936e3792506286a74c9dd283dd5a1de819bf3d8
deepily <42214371+deepily@users.noreply.github.com>
2024-03-25 18:06:49 -0400

    Add search for KNN by query string similarity

--- src/lib/memory/query_and_response_table.py
+++ src/lib/memory/query_and_response_table.py
@@ -1,8 +1,9 @@
 import lib.utils.util               as du
 
-from lib.memory.solution_snapshot  import SolutionSnapshot as ss
-from lib.app.configuration_manager import ConfigurationManager
-from lib.utils.util_stopwatch      import Stopwatch
+from lib.memory.question_embeddings_table import QuestionEmbeddingsTable
+from lib.memory.solution_snapshot         import SolutionSnapshot as ss
+from lib.app.configuration_manager        import ConfigurationManager
+from lib.utils.util_stopwatch             import Stopwatch
 
 import lancedb
 
@@ -37,7 +38,8 @@ class QueryAndResponseTable():
         
         self.db = lancedb.connect( uri )
         
-        self._query_and_response_tbl = self.db.open_table( "query_and_response_tbl" )
+        self._query_and_response_tbl  = self.db.open_table( "query_and_response_tbl" )
+        self._question_embeddings_tbl = QuestionEmbeddingsTable( debug=self.debug, verbose=self.verbose )
 
         print( f"Opened query_and_response_tbl w/ [{self._query_and_response_tbl.count_rows()}] rows" )
 
@@ -49,12 +51,15 @@ class QueryAndResponseTable():
         
     def insert_row( self, date=du.get_current_date(), time=du.get_current_time( include_timezone=False), query="", query_embedding=[], response_raw="", response_raw_embedding=[], response_conversational="", response_conversational_embedding=[], solution_path_wo_root=None ):
         
+        # ¡OJO! The embeddings are optional. If not provided, they will be generated.
+        # In this case the only embedding that we are cashing is the one that corresponds to the query, otherwise known
+        # as the 'question' in the solution snapshot object and the 'query' in the self._question_embeddings_tbl object.
         timer = Stopwatch( msg="insert_row() called..." )
         new_row = [ {
             "date"                             : date,
             "time"                             : time,
             "query"                            : query,
-            "query_embedding"                  : query_embedding if query_embedding else ss.generate_embedding( query ),
+            "query_embedding"                  : query_embedding if query_embedding else self._question_embeddings_tbl.get_embedding( query ),
             "response_raw"                     : response_raw,
             "response_raw_embedding"           : response_raw_embedding if response_raw_embedding else ss.generate_embedding( response_raw ),
             "response_conversational"          : response_conversational,
@@ -64,6 +69,27 @@ class QueryAndResponseTable():
         self._query_and_response_tbl.add( new_row )
         timer.print( "Done!", use_millis=True )
     
+    def get_knn_by_query( self, search_terms, k=5 ):
+        
+        timer = Stopwatch( msg="get_knn_by_query() called..." )
+        
+        # First, convert the search_terms string into an embedding. The embedding table caches all question embeddings
+        search_terms_embedding = self._question_embeddings_tbl.get_embedding( search_terms )
+        
+        knn = self._query_and_response_tbl.search(
+            search_terms_embedding, vector_column_name="query_embedding"
+        ).metric( "dot" ).limit( k ).select( [ "query", "response_conversational" ] ).to_list()
+        timer.print( "Done!", use_millis=True )
+        
+        if self.debug and self.verbose:
+            # Compare the embeddings for the search_terms and for the query_embedding fields
+            for i in range( 32 ):
+                print( knn[ 0 ][ "query_embedding" ][ i ] == search_terms_embedding[ i ], knn[ 0 ][ "query_embedding" ][ i ], search_terms_embedding[ i ] )
+    
+            # Are the search term embeddings and the query embeddings equal?
+            print( 'knn[ 0 ][ "query_embedding" ] == search_terms_embedding:', knn[ 0 ][ "query_embedding" ] == search_terms_embedding )
+        
+        return knn
     def init_tbl( self ):
 
         self.db.drop_table( "query_and_response_tbl" )
@@ -91,11 +117,8 @@ class QueryAndResponseTable():
         # self._query_and_response_tbl.add( df_dict )
         # print( f"New: Table.count_rows: {self._query_and_response_tbl.count_rows()}" )
         
-        du.print_banner( "Tables:" )
-        print( self.db.table_names() )
-        # du.print_banner( "Table:" )
-        # print( self._query_and_response_tbl.head( 10 ) )
-        #
+        # du.print_banner( "Tables:" )
+        # print( self.db.table_names() )
         # schema = self._query_and_response_tbl.schema
         #
         # du.print_banner( "Schema:" )
@@ -106,26 +129,45 @@ class QueryAndResponseTable():
         response_raw = "Same as it ever was, same as it ever was. Same as it ever was, same as it ever was. Same as it ever was, same as it ever was. Same as it ever was, same as it ever was."
         response_conversational = "Same as it ever was"
         self.insert_row( query=query, response_raw=response_raw, response_conversational=response_conversational )
+        
+        query = "what time is it"
+        response_raw = "14:45:00 EST"
+        response_conversational = "It's 2:45 PM EST."
+        self.insert_row( query=query, response_raw=response_raw, response_conversational=response_conversational )
+        
+        query = "what day is today"
+        response_raw = "03/25/2024"
+        response_conversational = "Today is March 25th, 2024."
+        self.insert_row( query=query, response_raw=response_raw, response_conversational=response_conversational )
+        
         print( f"AFTER: Table.count_rows: {self._query_and_response_tbl.count_rows()}" )
         
-        querys = [ query ] + [ "what time is it", "well how did I get here" ]
-        timer = Stopwatch()
-        for query in querys:
-            results = self._query_and_response_tbl.search().where( f"query = '{query}'" ).limit( 1 ).select( [ "date", "time", "query", "response_conversational", "response_raw" ] ).to_list()
-            du.print_banner( f"Synonyms for '{query}': {len( results )} found" )
-            for result in results:
-                print( f"Date: [{result[ 'date' ]}], Time: [{result[ 'time' ]}], Query: [{result[ 'query' ]}], Response: [{result[ 'response_conversational' ]}] Raw: [{result[ 'response_raw' ]}]" )
-
-        timer.print( "Search time", use_millis=True )
-        delta_ms = timer.get_delta_ms()
-        print( f"Average search time: {delta_ms / len( querys )} ms" )
-
-        # result = self._query_and_response_tbl.search( "what time is it" ).limit( 2 ).to_pandas()
-        # print( result )
+        # querys = [ query ] + [ "what time is it", "what day is today", "well how did I get here" ]
+        # timer = Stopwatch()
+        # for query in querys:
+        #     results = self._query_and_response_tbl.search().where( f"query = '{query}'" ).limit( 1 ).select( [ "date", "time", "query", "response_conversational", "response_raw" ] ).to_list()
+        #     du.print_banner( f"Synonyms for '{query}': {len( results )} found" )
+        #     for result in results:
+        #         print( f"Date: [{result[ 'date' ]}], Time: [{result[ 'time' ]}], Query: [{result[ 'query' ]}], Response: [{result[ 'response_conversational' ]}] Raw: [{result[ 'response_raw' ]}]" )
+        #
+        # timer.print( "Search time", use_millis=True )
+        # delta_ms = timer.get_delta_ms()
+        # print( f"Average search time: {delta_ms / len( querys )} ms" )
         
 if __name__ == '__main__':
     
-    query_and_response_tbl = QueryAndResponseTable( debug=True)
+    import numpy as np
+    foo = ss.generate_embedding( "what time is it" )
+    print( "Sum of foo", np.sum( foo ) )
+    print( "dot product of foo and foo", np.dot( foo, foo ) * 100 )
+    
+    query_and_response_tbl = QueryAndResponseTable( debug=True )
+    # query_and_response_tbl.init_tbl()
+    results = query_and_response_tbl.get_knn_by_query( "what time is it", k=5 )
+
+    for row in results:
+        print( row[ "query" ], row[ "response_conversational" ], row[ "_distance" ] )
+    
     # query_and_response_tbl.init_tbl()
     # query_1 = "what time is it"
     # print( f"'{query_1}': in embeddings table [{query_and_response_tbl.is_in( query_1 )}]" )
