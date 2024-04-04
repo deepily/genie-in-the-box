ebca3442d308f458cf10f62309377c718f7d7fd7
deepily <42214371+deepily@users.noreply.github.com>
2024-04-04 15:14:12 -0400

    Tweak code format

--- src/lib/agents/runnable_code.py
+++ src/lib/agents/runnable_code.py
@@ -50,7 +50,6 @@ class RunnableCode:
         
         return self.code_response_dict is not None and self.code_response_dict.get( "return_code", -1 ) == 0
     
-    
     def get_code_and_metadata( self ):
         
         return self.code_response_dict
